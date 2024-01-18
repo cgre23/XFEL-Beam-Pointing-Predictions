bcb96ed92940b88d26d62972acd9125668002803
Christian Grech <christian.grech@desy.de>
2024-01-18 12:39:37 +0100

    Update pydoocs_read.py

--- modules/pydoocs_read.py
+++ modules/pydoocs_read.py
@@ -3,11 +3,12 @@ import pydoocs
 
 if sys.argv[1]=='SA1':
     print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA1/TRAIN_MODEL_DATE')['data'])
+    pydoocs.write('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA1/TRAIN_MODEL_STATUS', 'TRAINING')
     #print('2023-11-27')
 if sys.argv[1]=='SA2':
     print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA2/TRAIN_MODEL_DATE')['data'])
-    #pass
+    pydoocs.write('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA2/TRAIN_MODEL_STATUS', 'TRAINING')
 if sys.argv[1]=='SA3':
     print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA3/TRAIN_MODEL_DATE')['data'])
-    #pass
+    pydoocs.write('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA3/TRAIN_MODEL_STATUS', 'TRAINING')
 sys.exit(0)
