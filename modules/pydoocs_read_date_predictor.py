f136d02f04e4cfcd8261a44627aff8ebb3bcd985
Local xfeloper account <xfeloper@mcsconeval.desy.de>
2024-02-05 12:59:14 +0100

    Changed pydoocs read script

--- modules/pydoocs_read_date_predictor.py
+++ modules/pydoocs_read_date_predictor.py
@@ -2,13 +2,10 @@ import sys
 import pydoocs
 
 if sys.argv[1]=='SA1':
-    print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA1/TRAIN_MODEL_DATE')['data'])
-    pydoocs.write('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA1/TRAIN_MODEL_STATUS', 'TRAINING')
+    print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA1/UPDATE_MODEL_DATE')['data'])
     #print('2023-11-27')
 if sys.argv[1]=='SA2':
-    print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA2/TRAIN_MODEL_DATE')['data'])
-    pydoocs.write('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA2/TRAIN_MODEL_STATUS', 'TRAINING')
+    print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA2/UPDATE_MODEL_DATE')['data'])
 if sys.argv[1]=='SA3':
-    print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA3/TRAIN_MODEL_DATE')['data'])
-    pydoocs.write('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA3/TRAIN_MODEL_STATUS', 'TRAINING')
+    print(pydoocs.read('XFEL.UTIL/DYNPROP/BEAM_PREDICT.SA3/UPDATE_MODEL_DATE')['data'])
 sys.exit(0)
