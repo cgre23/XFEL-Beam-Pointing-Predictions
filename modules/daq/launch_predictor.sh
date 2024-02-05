bec1dc41b084d679b8356064f429e17f4f0cfb13
Christian Grech <christian.grech@desy.de>
2024-02-05 11:45:15 +0100

    add predictor reading date script

--- modules/daq/launch_predictor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SASE='SA1'
-
-cd /home/xfeloper/user/chgrech/xfel-daq-ui/modules
-source venv/bin/activate
-export PYTHONPATH=$PYTHONPATH_OLD:/home/xfeloper.nfs/user/chgrech/xfel-daq-ui/modules/venv/lib/python3.9:/home/doocsadm/bm/python/DAQ/classes:/export/doocs/lib:/net/doocsdev16/export/doocs/lib:$PYTHONPATH;
-date=`python pydoocs_read.py "$SASE"`
-nohup python -m dxmaf -c docs/datalog_SA1.conf
-conda deactivate
--- modules/launch_predictor.sh
+++ modules/launch_predictor.sh
@@ -4,6 +4,8 @@ SASE='SA1'
 cd /home/xfeloper/user/chgrech/xfel-daq-ui/modules
 source venv/bin/activate
 export PYTHONPATH=$PYTHONPATH_OLD:/home/xfeloper.nfs/user/chgrech/xfel-daq-ui/modules/venv/lib/python3.9:/local/lib:/home/xfeloper/released_software/python/hlc_toolbox_common:/home/xfeloper/released_software/python/lib:$PYTHONPATH;
-date=`python pydoocs_read.py "$SASE"`
+date=`python pydoocs_read_date_predictor.py "$SASE"`
+
+cd daq
 nohup python -m dxmaf -c docs/datalog_SA1.conf
 conda deactivate
