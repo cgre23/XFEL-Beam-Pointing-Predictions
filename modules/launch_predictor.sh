f7e8335939cc5a17fa16415570ef2e128743caa1
XFEL Operator <xfeloper@xfelbkr1.desy.de>
2024-02-25 05:53:48 +0100

    Image analysis server changed location

--- modules/launch_predictor.sh
+++ modules/launch_predictor.sh
@@ -3,7 +3,7 @@ SASE='SA1'
 
 cd /home/xfeloper/user/chgrech/xfel-daq-ui/modules
 source venv/bin/activate
-export PYTHONPATH=$PYTHONPATH_OLD:/home/xfeloper.nfs/user/chgrech/xfel-daq-ui/modules/venv/lib/python3.9:/local/lib:/home/xfeloper/released_software/python/hlc_toolbox_common:/home/xfeloper/released_software/python/lib:$PYTHONPATH;
+#export PYTHONPATH=$PYTHONPATH_OLD:/home/xfeloper.nfs/user/chgrech/xfel-daq-ui/modules/venv/lib/python3.9:/local/lib:/home/xfeloper/released_software/python/hlc_toolbox_common:/home/xfeloper/released_software/python/lib:$PYTHONPATH;
 
 cd daq
 python -m dxmaf -c docs/datalog_SA1.conf
